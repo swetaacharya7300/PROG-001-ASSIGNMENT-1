702a2c3e07fe950df6e3f2034ef5e22c795d71a9
swetaacharya7300 <140718155+swetaacharya7300@users.noreply.github.com>
2023-09-10 17:30:32 +1000

    Update StudentStatistics.java
    Function 3 updated

--- StudentStatistics.java
+++ StudentStatistics.java
@@ -8,29 +8,7 @@ import java.util.*;
 public class StudentStatistics
 {
     public static void main(String[] args) {
-        // F1: Scanner input object to read input from the user
-        Scanner input = new Scanner(System.in);
-
-        // let the user to enter the assignment name
-        System.out.print("Please Enter the Assignment Name: ");
-
-        // Read input from the user and store in a variable
-        String assignmentName = input.nextLine();
-        
        
-        // F2:  Allows the user to input 30 student's marks for the assignment.
-        double[] marks = new double[30]; 
-        
-        
-        for (int i = 0; i < marks.length; i++) {
-            
-            double mark;
-        
-            do {
-                
-                System.out.print("Enter the student " + (i+1) + " mark: ");
-                
-                mark = input.nextDouble();
                 //F3: showing an error message on the screen to the user with sufficient details of the error before allowing users to re-enter the mark.
                 if (mark < 0 || mark > 30) {
                     System.out.println("Invalid marks! Please enter the marks between 0 and 30");
