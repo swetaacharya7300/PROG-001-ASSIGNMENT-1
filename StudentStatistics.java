f13291a92ca4a21844822f6b58f0bd95c138d690
swetaacharya7300 <140718155+swetaacharya7300@users.noreply.github.com>
2023-09-10 12:45:09 +0545

    Function-2
    Allows the user to input students' marks for the assignment. In this task, you can assume that there are 30 students in the unit. Your program 
    must accept only valid mark inputs by users. A valid mark is any number between 0 and 30.

--- StudentStatistics.java
+++ StudentStatistics.java
@@ -18,6 +18,30 @@ public class StudentStatistics
       //Read input from the user and store in a variable.
       String assessmentName = input.nextLine();
       
-    
+    //F2:  Allows the user to input 30 student's marks for the assignment.
+      double [] marks= new double[30];
+      
+      for(int i=0; i<marks.length; i++){
+          
+          double mark;
+          
+          do{
+              
+              System.out.println("Enter the student " +(i+1)+ "mark: ");
+              
+              mark = input.nextDouble();
+              
+              //F3: showing an error message on the screen to the user with sufficient details of the error before allowing users to re-enter the mark.
+              
+              if(mark<0 || mark>30){
+                  
+                  System.out.println("Invalid marks!The marks should be between 0 and 30! ");
+              }
+          }
+          while(mark < 0 || mark > 30);
+          marks[i] = mark;
+          
+      }
+      
 }
 }
