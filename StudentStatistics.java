314d8767fcef25f4a9a9ee1477f0f48b5ecc7e62
swetaacharya7300 <140718155+swetaacharya7300@users.noreply.github.com>
2023-09-10 13:13:07 +0545

    Function 3
     eports any invalid input by showing an error message on the screen to the user with sufficient details of the error before allowing users to re-enter the mark.

--- StudentStatistics.java
+++ StudentStatistics.java
@@ -7,41 +7,49 @@ import java.util.*;
  */
 public class StudentStatistics
 {
+    public static void main(String[] args) {
+        // F1: Scanner input object to read input from the user
+        Scanner input = new Scanner(System.in);
+
+        // let the user to enter the assignment name
+        System.out.print("Please Enter the Assignment Name: ");
+
+        // Read input from the user and store in a variable
+        String assignmentName = input.nextLine();
+        
+       
+        // F2:  Allows the user to input 30 student's marks for the assignment.
+        double[] marks = new double[30]; 
+        
+        
+        for (int i = 0; i < marks.length; i++) {
+            
+            double mark;
+        
+            do {
+                
+                System.out.print("Enter the student " + (i+1) + " mark: ");
+                
+                mark = input.nextDouble();
+                //F3: showing an error message on the screen to the user with sufficient details of the error before allowing users to re-enter the mark.
+                if (mark < 0 || mark > 30) {
+                    System.out.println("Invalid marks! Please enter the marks between 0 and 30");
+                } 
+                
+            } while (mark < 0 || mark > 30);
+            
+             marks[i] = mark;
+        }
+        
+         
+    }
     
-  public static void main(String[] args){
-      // F1: Scanner input objects to read input from the user.
-      Scanner input = new Scanner(System.in);
-      
-      //let the user to enter the assignment name
-      System.out.println("Enter the Assessment Name: ");
-      
-      //Read input from the user and store in a variable.
-      String assessmentName = input.nextLine();
-      
-    //F2:  Allows the user to input 30 student's marks for the assignment.
-      double [] marks= new double[30];
-      
-      for(int i=0; i<marks.length; i++){
-          
-          double mark;
-          
-          do{
-              
-              System.out.println("Enter the student " +(i+1)+ "mark: ");
-              
-              mark = input.nextDouble();
-              
-              //F3: showing an error message on the screen to the user with sufficient details of the error before allowing users to re-enter the mark.
-              
-              if(mark<0 || mark>30){
-                  
-                  System.out.println("Invalid marks!The marks should be between 0 and 30! ");
-              }
-          }
-          while(mark < 0 || mark > 30);
-          marks[i] = mark;
-          
-      }
-      
 }
-}
+    
+      
+    
+          
+      
+      
+
+
