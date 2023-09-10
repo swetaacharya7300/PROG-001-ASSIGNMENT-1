0012d12b621b99c87af67b662516faa3ff5392df
swetaacharya7300 <140718155+swetaacharya7300@users.noreply.github.com>
2023-09-10 13:19:26 +0545

    Function 4
    Prints the assignment name following the 'students' marks after users have finished entering the marks.

--- StudentStatistics.java
+++ StudentStatistics.java
@@ -8,7 +8,29 @@ import java.util.*;
 public class StudentStatistics
 {
     public static void main(String[] args) {
+        // F1: Scanner input object to read input from the user
+        Scanner input = new Scanner(System.in);
+
+        // let the user to enter the assignment name
+        System.out.print("Please Enter the Assignment Name: ");
+
+        // Read input from the user and store in a variable
+        String assignmentName = input.nextLine();
+        
        
+        // F2:  Allows the user to input 30 student's marks for the assignment.
+        double[] marks = new double[30]; 
+        
+        
+        for (int i = 0; i < marks.length; i++) {
+            
+            double mark;
+        
+            do {
+                
+                System.out.print("Enter the student " + (i+1) + " mark: ");
+                
+                mark = input.nextDouble();
                 //F3: showing an error message on the screen to the user with sufficient details of the error before allowing users to re-enter the mark.
                 if (mark < 0 || mark > 30) {
                     System.out.println("Invalid marks! Please enter the marks between 0 and 30");
@@ -18,6 +40,15 @@ public class StudentStatistics
             
              marks[i] = mark;
         }
+        // F4
+         System.out.println("Student Marks entered Successfully!");
+         System.out.println("Assignment Name: " + assignmentName);
+         System.out.println("Students' Entered Marks are given Below:");
+        
+        for (int i = 0; i < 30; i++) {
+            
+            System.out.println("Student "+ (i+1) + " Mark: " + marks[i]);
+        }         
         
          
     }
