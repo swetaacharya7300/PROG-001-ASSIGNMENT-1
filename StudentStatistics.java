3235337d1c08905f20a4832ce8a5cde221f52cf8
swetaacharya7300 <140718155+swetaacharya7300@users.noreply.github.com>
2023-09-10 12:41:56 +0545

    Function-1
    Allows the user to input the assignment name.

---
+++ StudentStatistics.java
@@ -0,0 +1,23 @@
+import java.util.*;
+/**
+ *Program to compute statistics of 'students' marks in an assignment.
+ *
+ * @author (Sweta)
+ * @version (09/09/2023)
+ */
+public class StudentStatistics
+{
+    
+  public static void main(String[] args){
+      // F1: Scanner input objects to read input from the user.
+      Scanner input = new Scanner(System.in);
+      
+      //let the user to enter the assignment name
+      System.out.println("Enter the Assessment Name: ");
+      
+      //Read input from the user and store in a variable.
+      String assessmentName = input.nextLine();
+      
+    
+}
+}
